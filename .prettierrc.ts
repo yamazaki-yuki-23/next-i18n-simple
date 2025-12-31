6842ca610fb1d77900a3be5d9191f4a8c1f25bb2
yamazaki-yuki-23 <natural4083@gmail.com>
2025-12-31 17:33:27 +0900

    package.jsonとyarn.lockを更新し、Prettierとeslint-plugin-importを追加しました。また、Prettierのフォーマットスクリプトを追加しました。

---
+++ .prettierrc.ts
@@ -0,0 +1,10 @@
+import { type Config } from 'prettier'
+
+const config: Config = {
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'none'
+}
+
+export default config
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,36 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals'
-import nextTs from "eslint-config-next/typescript";
+import nextTs from 'eslint-config-next/typescript'
 import prettier from 'eslint-config-prettier/flat'
+import importPlugin from 'eslint-plugin-import'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  prettier,
   ...nextTs,
+  {
+    plugins: {
+      import: importPlugin
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true }
+        }
+      ]
+    }
+  },
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     '.next/**',
     'out/**',
     'build/**',
-    'next-env.d.ts',
-  ]),
+    'next-env.d.ts'
+  ])
 ])
- 
+
 export default eslintConfig
