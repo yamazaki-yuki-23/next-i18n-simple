b16f2c4d2a0f361d95659badfaae0826cab1b850
yamazaki-yuki-23 <natural4083@gmail.com>
2026-01-10 23:29:29 +0900

    jestからvitestへ移行

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts',
+    include: ['**/*.test.ts', '**/*.test.tsx'],
+    exclude: ['node_modules', 'dist', '.next', 'coverage']
+  }
+})
